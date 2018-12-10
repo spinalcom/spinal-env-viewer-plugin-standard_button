570bcc18c0426c859d8e3cf4f252b6faefef10d2
TheCoolNickName <v.delassus@spinalcom.com>
2018-12-10 16:35:29 +0100

    fix bug sortButton

--- js/sortButton.js
+++ js/sortButton.js
@@ -31,6 +31,7 @@ const {
   SpinalForgeExtention
 } = require("spinal-env-viewer-panel-manager-service_spinalforgeextention");
 
+import { search_node } from './external.js';
 
 class SpinalContextSortByName extends SpinalContextApp {
   constructor() {
@@ -49,159 +50,9 @@ class SpinalContextSortByName extends SpinalContextApp {
   }
 
   action(option) {
-    console.log(option);
     search_node(option.selectedNode, this.sorted, option.selectedNode.info.name.get());
     this.sorted = !this.sorted;
   }
 }
-
-function search_node(node, bool, name) {
-   let el = document.getElementsByClassName("node-name");
-   let ite = 0;
-   let arrName = []
-
-
-   while (el[ite]) {
-    if (el[ite].innerText === name) {
-      //console.log("success");
-      //console.log(el[ite].parentElement.parentElement);
-      //console.log(el[ite].parentElement.parentElement.childNodes);
-     // el[ite].parentElement.parentElement.appendChild(el[ite].parentElement.parentElement.childNodes[4]);
-      sort_node(el[ite].parentElement.parentElement);
-      return;
-      // el[ite].parentElement.parentElement.replaceChild(el[ite].parentElement.parentElement.childNodes[4], el[ite].parentElement.parentElement.childNodes[5]);
-    }
-    ite++;
-   }
-
-   if (!bool)
-      console.log("a-z");
-   else
-     console.log("z-a");
-}
-
-function sort_node(target) {
-  let list = target;
-  let ite = 2;
-  let count = 0;
-  let index = 0;
-  let arrName = [];
-  console.log("------------------------");
-  target = target.childNodes;
-  console.log(target);
-  while(target[ite]) {
-  console.log(target[ite].children);
-  arrName.push(target[ite].innerText.replace(/\n[\w\W\d]*/gi, ''));
-    ite++;
-  }
-  ite = 2;
-  let echange;
-  console.log("----------end------------------");
-  console.log("arrName = ", arrName);
-  console.log("target = ",target);
-
-
-  let result = [];
-
-  while (arrName[ite - 2]) {
-    console.log(arrName[ite - 2]);
-    while(arrName[count]) {
-      if (arrName[count] < arrName[ite - 2]) {
-        console.log(arrName[ite-2],"<",arrName[count])
-        echange = arrName[ite - 2];
-        arrName[ite - 2] = arrName[count];
-        arrName[count] = echange;
-
-      //  echange = target[ite];
-      //  target[ite] = target[count+2]
-      //  target[count+2] = echange;
-
-
-
-        //echange = arrName[ite - 2];
-        //arrName[ite - 2] = arrName[count];
-        //arrName[count] = echange;
-      }
-      count++;
-    }
-    count = 0;
-    ite++;
-  }
-  console.log(arrName);
-  console.log(result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-let newArr = [];
-  while (arrName[ite-2]) {
-    while(arrName[count]) {
-      console.log(arrName[count], arrName[ite-2]);
-      console.log(arrName[count] < arrName[ite-2]);
-      console.log(target);
-      console.log(arrName);
-      console.log("--------------");
-      if (arrName[count] < arrName[ite-2]) {
-  
-
-
-        newArr[ite-2] = target[count + 2];
-        //console.log(arrName[count]);
-        echange = arrName[ite-2];
-        arrName[ite-2] = arrName[count];
-        arrName[count] = echange;
-        //target[1].insertBefore(target[2], );
-       // list.appendChild(target[count + 2]);
-        //index = count;
-
-
-      }
-      count++;
-      target.
-      //target = list.childNodes;
-    }
-    count = 0;
-    ite++;
-  }*/
-
-
-  //console.log("end");
- // console.log(newArr);
-  //target.
- // console.log(arrName);
- //console.log(target);
- //target.appendChild(newArr);
-}
-
+ 
 export { SpinalContextSortByName };
