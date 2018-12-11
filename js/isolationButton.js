60bd75d720f78292174d94ba99eb8c8f6b323bd0
TheCoolNickName <45173844+TheCoolNickName@users.noreply.github.com>
2018-12-11 11:50:18 +0100

    Update isolationButton.js

--- js/isolationButton.js
+++ js/isolationButton.js
@@ -42,7 +42,7 @@ class SpinalContextIsolation extends SpinalContextApp {
 
   isShown(option) {
   //  if (option.selectedNode instanceof spinalgraph.SpinalContext)
-      return (true);
+      return (Promise.resolve(true));
 //    else
 //      return (-1);
   }
